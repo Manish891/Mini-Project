9f2e9cfbcaf8e6fcbb81222f5e603f851209ed7a
Manish891 <siddhanthammanish5@gmail.com>
2025-03-19 14:33:34 +0530

    Update AccountInfo.jsx

--- Mini Project - 01/AccountInfo.jsx
+++ Mini Project - 01/AccountInfo.jsx
@@ -8,8 +8,6 @@ const AccountInfo = () => {
       <div className="account-details">
         <p><strong>Name:</strong> Manish</p>
         <p><strong>Email:</strong> msiddhantham@gmail.com</p>
-        <p><strong>Phone:</strong> +91 9876543210</p>
-        <p><strong>Address:</strong> Hyderabad, India</p>
       </div>
     </div>
   );
